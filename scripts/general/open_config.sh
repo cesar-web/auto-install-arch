7ec9cb2f0711a0995425e8d25edd939303a0977b
cesar-web <munozcesar.cm@gmail.com>
2021-10-04 15:05:44 -0500

    Added a preview to the scripts

--- scripts/general/open_config.sh
+++ scripts/general/open_config.sh
@@ -3,18 +3,20 @@
 # script is used to bind a key in tmux to a menu that opens a file
 file=$(echo "tmux vim.init bashrc zshrc xinitrc xbindkeysrc notes todo keybinds scripts->" | tr ' ' '\n' | fzf)
 case "$file" in
-    "tmux")        nvim ~/auto-install-arch/tmux.conf             ;;
-    "bashrc")      nvim ~/auto-install-arch/bashrc                ;;
-    "zshrc")       nvim ~/auto-install-arch/zshrc                 ;;
-    "xinitrc")     nvim ~/auto-install-arch/xinitrc               ;;
-    "xbindkeysrc") nvim ~/auto-install-arch/xbindkeysrc           ;;
-    "keybinds")    nvim ~/auto-install-arch/keyd.cfg              ;;
+    "tmux")        nvim ~/auto-install-arch/home/tmux.conf             ;;
+    "bashrc")      nvim ~/auto-install-arch/home/bashrc                ;;
+    "zshrc")       nvim ~/auto-install-arch/home/zshrc                 ;;
+    "xinitrc")     nvim ~/auto-install-arch/home/xinitrc               ;;
+    "xbindkeysrc") nvim ~/auto-install-arch/home/xbindkeysrc           ;;
+    "keybinds")    nvim ~/auto-install-arch/other/keyd.cfg              ;;
     "notes")       nvim ~/documents/text_files/notes              ;;
     "todo")        nvim ~/documents/text_files/TODO               ;;
     "vim.init")    nvim ~/auto-install-arch/.config/nvim/init.vim ;;
     "scripts->")
-        cd ~/scripts
-        script=$(find . -type f | fzf)
+        cd ~/auto-install-arch/scripts/
+        script=$(find . -type f | fzf \
+            --preview="dash ~/scripts/keybind/find_file/file_preview.sh {}" \
+            --preview-window="top,80%")
         if [ "$script" != '' ]; then
             nvim "$script"
         fi
